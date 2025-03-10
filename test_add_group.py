5fd364ca21d7813d4f91feb11902eb38bdba0df8
Laptev Roman <madkid1@mai.ru>
2025-03-10 14:51:02 +0600

    Код логина и создания группы с рекордера. driver изменен на wd

--- test_add_group.py
+++ test_add_group.py
@@ -1,4 +1,3 @@
-import unittest, time, re
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,32 +7,33 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-
-class UntitledTestCase(unittest.TestCase):
+class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-
-    def test_untitled_test_case(self):
+    
+    def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get("https://localhost/addressbook/#")
+        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_name("MainForm").click()
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("namegroup")
+        wd.find_element_by_name("group_name").send_keys("sdfsdfsdfdsfsdfdf")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("gogogogo")
+        wd.find_element_by_name("group_header").send_keys("dsfsdfsf")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("ezezeze")
+        wd.find_element_by_name("group_footer").send_keys("sdfsdfsdf")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
@@ -47,7 +47,7 @@ class UntitledTestCase(unittest.TestCase):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-
+    
     def tearDown(self):
         self.wd.quit()
 
